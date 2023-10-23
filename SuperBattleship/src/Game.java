f1a766246b997183c55a65975cc7328191ff1f5d
Zachary Wiebe <zachary.wiebe@msubillings.edu>
2023-10-23 14:22:11 -0600

    Rewrote the Game Class

---
+++ SuperBattleship/src/Game.java
@@ -0,0 +1,36 @@
+public class game(){
+
+    // INSTANCE VARIBLES
+    
+    List<Player> players;
+    Player currentPlayer;
+    Player opposingPlayer;
+
+    // PUBLIC METHODS
+
+    public void play(){
+
+    }
+
+    public void config(){
+
+    }
+
+    // PRIVATE METHODS
+
+    private void configPlayers(){
+
+    }
+
+    private void tutorialOption(){
+
+    }
+
+    private void formatNames(){
+
+    }
+
+    private void clear(){
+
+    }
+}
