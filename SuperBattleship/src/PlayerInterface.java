79fb9612e09e8e893714eafa85713c74604a8ea2
Zachary Wiebe <zachary.wiebe@msubillings.edu>
2023-10-23 14:07:34 -0600

    Created Player/Game/Ascii Art Class "skeletons"

---
+++ SuperBattleship/src/PlayerInterface.java
@@ -0,0 +1,9 @@
+package SuperBattleship.src;
+
+interface PlayerInterface {
+    public Coord takeTurn();
+    public ShotResult recieveShot();
+    public void recieveShotResult();
+    public Boolean validateTurn();
+    public String getName();
+}
