f326efb7a995d17022217c07e7c1ccae2bbdedbe
Taylor Branson <taylorbranson2000@gmail.com>
2023-10-24 12:25:56 -0600

    More poppin

--- SuperBattleship/src/App.java
+++
@@ -1,8 +0,0 @@
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        OceanGrid oGrid = new OceanGrid();
-        TargetGrid tGrid = new TargetGrid();
-        System.out.println(oGrid.print() + "\n\n\n" + tGrid.print());
-    }
-}
--- SuperBattleship/src/PlayerInterface.java
+++
@@ -1,9 +0,0 @@
-package SuperBattleship.src;
-
-interface PlayerInterface {
-    public Coord takeTurn();
-    public ShotResult recieveShot();
-    public void recieveShotResult();
-    public Boolean validateTurn();
-    public String getName();
-}
