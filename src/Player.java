a3bdc11528d7fbb30bf717a4814e0e8b36e42abe
Ayjeren Kossekova <ayjeren.kossekova@msubillings.edu>
2023-10-25 14:57:28 -0600

    updated Player class 2.0

--- src/Player.java
+++ src/Player.java
@@ -13,10 +13,7 @@ public class Player  implements PlayerInterface{
     public String getName() {
         return name;
     }
-
-    public ShotResult takeShot(Coord shot) {
-        return oceanGrid.receiveShot(shot);
-    }
+    
     @Override
     public Coord takeTurn() {
         Coord shot = null;
